021cf11df62da472582390ba69287503f8571323
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2017-08-24 15:06:12 -0700

    Added Geo API check

--- app.js
+++ app.js
@@ -25,17 +25,42 @@ server.post('/api/messages', connector.listen())
 
 // START TomTom API related Functions
 
+
 // Convert String Address to Geo Location
-var getGeo = function(location,func) {
+var getGeo = function (location, func) {
     // Return into func the actual Geo value of the string address.
 
-    func( location );
+    request("https://api.tomtom.com/search/2/geocode/" + encodeURI(location) + ".json?key="+apiKey, function (error, response, body) {
+        //console.log('error:', error); // Print the error if one occurred 
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+        //console.log('body:', body); // Print what was returned
+
+        var value = JSON.parse(body);
+
+        var position;
+
+        if (value) {
+            if (value.results) {
+                if (value.results[0]) {
+                    if (value.results[0].position) {
+                        position = value.results[0].position.lat + "," + value.results[0].position.lon
+                    }
+                }
+            }
+        }
+
+        if (position)
+            func(position);
+        else
+            func("ERROR"); // TO DO: Add better error messaging
+    });
+
 }
 
 // Bot dialog to display results
 bot.dialog('/results', [
-    function (session,route) {
-        console.log("DEBUG INFO: User "+session.message.user.id+" route var dump: "+JSON.stringify(route));
+    function (session, route) {
+        console.log("DEBUG INFO: User " + session.message.user.id + " route var dump: " + JSON.stringify(route));
 
         var dateObject = new Date(route.time);
 
@@ -53,8 +78,7 @@ bot.dialog('/', [
     function (session) {
         session.send("Hello! Welcome to the TomTom Online Routing API Bot Framework demo. I can tell you when you need to leave in order to arrive at your destination on time.");
 
-        if (session.userData.route == null)
-        {
+        if (session.userData.route == null) {
             session.userData.route = {}
         }
         session.beginDialog("/demo")
@@ -66,27 +90,27 @@ bot.dialog('/demo', [
     function (session) {
         //console.log("DEBUG INFO: User "+session.message.user.id+" Userdata dump: "+JSON.stringify(session.userData.route));
 
-        session.beginDialog("/getStartLocation",session.userData.route.start);
+        session.beginDialog("/getStartLocation", session.userData.route.start);
     },
     function (session, results) {
-        session.send("You are starting your journey from "+session.userData.route.start);
-    
-        session.beginDialog("/getDestLocation",session.userData.route.dest);
+        session.send("You are starting your journey from " + session.userData.route.start);
+
+        session.beginDialog("/getDestLocation", session.userData.route.dest);
     },
     function (session, results) {
-        session.send("You will be traveling to "+session.userData.route.dest);
+        session.send("You will be traveling to " + session.userData.route.dest);
 
         session.beginDialog("/getTime");
     },
     function (session, results) {
-        session.send("You want to arrive by "+session.userData.route.destTime);
-        
-        session.beginDialog("/results",session.userData.route);
+        session.send("You want to arrive by " + session.userData.route.destTime);
+
+        session.beginDialog("/results", session.userData.route);
     },
     function (session, results) {
-        
-        builder.Prompts.choice(session,"Would you like to calculate another route?",["Yes","Yes, but forget everything I told you before","No, not right now"])
-        
+
+        builder.Prompts.choice(session, "Would you like to calculate another route?", ["Yes", "Yes, but forget everything I told you before", "No, not right now"])
+
     },
     function (session, results) {
         if (results.response.entity == "Yes") {
@@ -107,41 +131,38 @@ bot.dialog('/demo', [
 // A dialog to get a starting location
 bot.dialog('/getStartLocation', [
     function (session, startLocation, next) {
-        session.dialogData.startLocation = startLocation;
-        if (session.dialogData.startLocation)
-        {
-            builder.Prompts.confirm(session, "Are you starting your journey from \""+session.dialogData.startLocation+"\" again?");
+        if (startLocation) {
+            builder.Prompts.confirm(session, "Are you starting your journey from \"" + startLocation + "\" again?");
         }
-        else
-        {
+        else {
             next(); // Skip to asking if we don't have the location
         }
     },
     function (session, results) {
         if (results.response) {
-            session.endDialogWithResult({ 
-                response: { startLocation: session.dialogData.startLocation } 
-            }); 
+            session.endDialogWithResult({
+                response: { startLocation: startLocation }
+            });
         }
-        else
-        {
+        else {
             builder.Prompts.text(session, "Enter an address you'd like to start from:");
         }
     },
     function (session, results) {
         if (results.response) {
-            session.dialogData.startLocation = results.response;
-        }
-        
-        if (session.dialogData.startLocation) {
-            session.userData.route.start = session.dialogData.startLocation;
+            getGeo(results.response, function (startGeo) {
+                if (startGeo == "ERROR") {
+                    session.send("ERROR! Could not validate address.")
+                    session.replaceDialog('/getStartLocation');
+                }
+                else {
+                    session.userData.route.start = results.response;
+                    session.userData.route.startGeo = startGeo;
 
-            getGeo(session.userData.route.start, function (startGeo) { 
-                session.userData.route.startGeo = startGeo;
-
-                session.endDialogWithResult({ 
-                    response: { startLocation: session.dialogData.startLocation } 
-                }); 
+                    session.endDialogWithResult({
+                        response: { startLocation: results.response }
+                    });
+                }
             });
         } else {
             session.endDialogWithResult({
@@ -154,41 +175,38 @@ bot.dialog('/getStartLocation', [
 // A dialog to get a destination
 bot.dialog('/getDestLocation', [
     function (session, destLocation, next) {
-        session.dialogData.destLocation = destLocation;
-        if (session.dialogData.destLocation)
-        {
-            builder.Prompts.confirm(session, "Are you traveling to \""+session.dialogData.destLocation+"\" again?");
+        if (destLocation) {
+            builder.Prompts.confirm(session, "Are you traveling to \"" + destLocation + "\" again?");
         }
-        else
-        {
+        else {
             next(); // Skip to asking if we don't have the location
         }
     },
     function (session, results) {
         if (results.response) {
-            session.endDialogWithResult({ 
-                response: { destLocation: session.dialogData.destLocation } 
-            }); 
+            session.endDialogWithResult({
+                response: { destLocation: destLocation }
+            });
         }
-        else
-        {
+        else {
             builder.Prompts.text(session, "Enter the address of where you'd like to go:");
         }
     },
     function (session, results) {
         if (results.response) {
-            session.dialogData.destLocation = results.response;
-        }
-        
-        if (session.dialogData.destLocation) {
-            session.userData.route.dest = session.dialogData.destLocation;
-            
-            getGeo(session.userData.route.dest, function (destGeo) { 
-                session.userData.route.destGeo = destGeo;
+            getGeo(results.response, function (destGeo) {
+                if (destGeo == "ERROR") {
+                    session.send("ERROR! Could not validate address.")
+                    session.replaceDialog('/getDestLocation');
+                }
+                else {
+                    session.userData.route.dest = results.response;
+                    session.userData.route.destGeo = destGeo;
 
-                session.endDialogWithResult({ 
-                    response: { destLocation: session.dialogData.destLocation } 
-                }); 
+                    session.endDialogWithResult({
+                        response: { destLocation: results.response }
+                    });
+                }
             });
         } else {
             session.endDialogWithResult({
@@ -206,7 +224,8 @@ bot.dialog('/getTime', [
     function (session, results) {
         if (results.response) {
             var t = builder.EntityRecognizer.resolveTime([results.response]);
-            session.dialogData.time = t.toISOString(); //Storing date object as String
+            if (t)
+                session.dialogData.time = t.toISOString(); //Storing date object as String
             // To get back as Date object: var dateObject = new Date(session.userData.route.time);
         }
 
@@ -214,9 +233,9 @@ bot.dialog('/getTime', [
         if (session.dialogData.time) {
             session.userData.route.destTime = session.dialogData.time;
 
-            session.endDialogWithResult({ 
-                response: { time: session.dialogData.time } 
-            }); 
+            session.endDialogWithResult({
+                response: { time: session.dialogData.time }
+            });
         } else {
             session.endDialogWithResult({
                 resumed: builder.ResumeReason.notCompleted
