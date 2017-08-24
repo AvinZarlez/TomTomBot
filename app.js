da3dec962574450b4019090b2afbd45315e7c9c7
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2017-08-24 11:08:18 -0700

    Added destLocation

--- app.js
+++ app.js
@@ -21,16 +21,24 @@ server.post('/api/messages', connector.listen())
 
 bot.dialog('/', [
     function (session) {
-        session.send(session.userData.startLocation+"Hello! Welcome to the TomTom Online Routing API Bot Framework demo. I can tell you when you need to leave in order to arrive at your destination on time.");
+        session.send("DEBUG: Userdata dump: {startLocation: "+session.userData.startLocation+", destLocation: "+session.userData.destLocation+"}");
+
+        session.send("Hello! Welcome to the TomTom Online Routing API Bot Framework demo. I can tell you when you need to leave in order to arrive at your destination on time.");
     
         session.beginDialog("/getStartLocation",session.userData.startLocation);
     },
     function (session, results) {
-        session.send(session.userData.startLocation+" is what the start user date is and you entered "+results.response.startLocation);
+        session.send("You are starting your journey from "+session.userData.startLocation);
+    
+        session.beginDialog("/getDestLocation",session.userData.destLocation);
+    },
+    function (session, results) {
+        session.send("You will be traveling to "+session.userData.startLocation);
+
         session.beginDialog("/getTime");
     },
     function (session, results) {
-        session.send(session.dialogData.time+" is what the dialog data is and you entered "+results.response.time);
+        session.send("You want to arrive by "+results.response.time);
         
         /*if (results.response) {
             session.replaceDialog('/loop', session.dialogData.save)
@@ -101,19 +109,63 @@ bot.dialog('/getStartLocation', [
     }
 ]);
 
-bot.dialog('/getTime', [
-    function (session) {
-        builder.Prompts.time(session, "What time would you like to set an alarm for?");
+bot.dialog('/getDestLocation', [
+    function (session, destLocation, next) {
+        session.dialogData.destLocation = destLocation;
+        if (session.dialogData.destLocation)
+        {
+            builder.Prompts.confirm(session, "Are you traveling to \""+session.dialogData.destLocation+"\" again?");
+        }
+        else
+        {
+            next(); // Skip to asking if we don't have the location
+        }
     },
     function (session, results) {
         if (results.response) {
-            session.dialogData.time = builder.EntityRecognizer.resolveTime([results.response]);
+            session.endDialogWithResult({ 
+                response: { destLocation: session.dialogData.destLocation } 
+            }); 
+        }
+        else
+        {
+            builder.Prompts.text(session, "Enter the address of where you'd like to go:");
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.destLocation = results.response;
+        }
+        
+        if (session.dialogData.destLocation) {
+            session.userData.destLocation = session.dialogData.destLocation; //TODO: Geocash this!!!
+
+            session.endDialogWithResult({ 
+                response: { destLocation: session.dialogData.destLocation } 
+            }); 
+        } else {
+            session.endDialogWithResult({
+                resumed: builder.ResumeReason.notCompleted
+            });
+        }
+    }
+]);
+
+bot.dialog('/getTime', [
+    function (session) {
+        builder.Prompts.time(session, "What time would you like to get there by?");
+    },
+    function (session, results) {
+        if (results.response) {
+            var t = builder.EntityRecognizer.resolveTime([results.response]);
+            //session.dialogData.time = t.toISOString();
+            //var dateObject = new Date(session.userdata.time);
         }
 
         // Return time  
-        if (session.dialogData.time) {
+        if (t) {
             session.endDialogWithResult({ 
-                response: { time: session.dialogData.time } 
+                response: { time: t } 
             }); 
         } else {
             session.endDialogWithResult({
